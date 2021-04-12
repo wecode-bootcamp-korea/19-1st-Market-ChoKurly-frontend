7b0211d015ed54de18edccd972bf158bdaafa2f8
Dongee Seo <dongeeseo@Dongeeui-MacBook-Pro.local>
2021-04-12 20:18:40 +0900

    Add: 초기세티 수정완료

--- Routes.js
+++ Routes.js
@@ -1,9 +1,5 @@
-import React, { Component } from 'react'
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-  } from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Main from '';
 import Signup from '';
@@ -13,21 +9,20 @@ import ProductDetails from '';
 import Cart from '';
 
 class Routes extends Component {
-    render() {
-        return (
-                <Router>
-                    <Switch>
-                        <Route exact path="/main" component={Main} />
-                        <Route exact path="/signup" component={Signup} />
-                        <Route exact path="/login" component={Login} />
-                        <Route exact path="/productlist" component={ProductList} />
-                        <Route exact path="/productdetails" component={ProductDetails} />
-                        <Route exact path="/cart" component={Cart} />
-                    </Switch>
-                </Router>
-
-        )
-    }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/main" component={Main} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/productlist" component={ProductList} />
+          <Route exact path="/productdetails" component={ProductDetails} />
+          <Route exact path="/cart" component={Cart} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default Routes;
