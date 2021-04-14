835e9390c7b19890eb69b966ff06431fd08852b0
JunHyeongChae <78126381+JunHyeongChae@users.noreply.github.com>
2021-04-15 08:39:53 +0900

    Add:회원가입 완료페이지 구성

--- src/Routes.js
+++ src/Routes.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Main from '';
-import Signup from '';
-import Login from '';
-import ProductList from '';
-import ProductDetails from '';
-import Cart from '';
+// import Main from '';
+// import Signup from './Pages/Signup/Signup';
+import SignupFin from './Pages/Signup/SignupFin/SignupFin';
+// import Login from '';
+// import ProductList from '';
+// import ProductDetails from '';
+// import Cart from '';
 
 class Routes extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/main" component={Main} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/login" component={Login} />
+          {/* <Route exact path="/main" component={Main} />
+          <Route exact path="/signup" component={Signup} /> */}
+          <Route exact path="/signupfin" component={SignupFin} />
+          {/* <Route exact path="/login" component={Login} />
           <Route exact path="/productlist" component={ProductList} />
           <Route exact path="/productdetails" component={ProductDetails} />
-          <Route exact path="/cart" component={Cart} />
+          <Route exact path="/cart" component={Cart} /> */}
         </Switch>
       </Router>
     );
