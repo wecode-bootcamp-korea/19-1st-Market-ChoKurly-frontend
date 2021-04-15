be7cf0fecaab9f2a8e99ac486d4d419ede4f63db
Dongee Seo <dongeeseo@Dongeeui-MacBook-Pro.local>
2021-04-15 19:27:43 +0900

    Add: 상세페이지 썸네일 레이아웃 완성

--- src/Pages/ProductDetails/Components/Thumbnail/Thumbnail.js
+++ src/Pages/ProductDetails/Components/Thumbnail/Thumbnail.js
@@ -25,7 +25,7 @@ class Thumbnail extends Component {
   render() {
     return (
       <section className="top-view">
-        <div className="thumbnail-wrapper">
+        <article className="thumbnail-wrapper">
           <img
             src="../../../images/상세페이지 썸네일.jpg"
             alt="대표 이미지"
@@ -54,7 +54,11 @@ class Thumbnail extends Component {
                   <dl key={ele.id} className="goods-units">
                     <dt className="info">{ele.chartName}</dt>
                     <dd className="data">
-                      {ele.data}
+                      {ele.data.map(line => (
+                        <p className="data-sub" key={line}>
+                          {line}
+                        </p>
+                      ))}
                       <dd className="subdata">{ele.subdata}</dd>
                     </dd>
                   </dl>
@@ -74,30 +78,45 @@ class Thumbnail extends Component {
                   </select>
                   <ul className="list">
                     <li className="goods-one">
-                      <span>
-                        [리터 스포트]미니 초콜릿 믹스 18p
-                        <button className="delete">X</button>
-                      </span>
-                      <div className="count-btn">
-                        <span>
-                          <button onClick={this.handleIncrease}>+</button>
-                          <input
-                            type="number"
-                            value={this.state.number}
-                          ></input>
-                          <button onClick={this.handleDecrease}>-</button>
+                      <div className="added-goods-name">
+                        <span>[리터 스포트]미니 초콜릿 믹스 18p</span>
+                        <span className="del-btn">
+                          <button className="delete">X</button>
                         </span>
-                        <span></span>
+                      </div>
+                      <div className="goods-quantity">
+                        <form className="count-btn">
+                          <button onClick={this.handleDecrease}>--</button>
+                          <input type="text" value={this.state.number}></input>
+                          <button onClick={this.handleIncrease}>+</button>
+                          <span></span>
+                        </form>
+                        <div className="fixed-price">
+                          <span className="goods-own-price">10,500</span>
+                          <span>원</span>
+                        </div>
                       </div>
                     </li>
                   </ul>
                 </div>
               </div>
             </div>
-            <div className="buy-for-it"></div>
+            <div className="buy-for-it">
+              <div className="total-price-wrapper">
+                <span> 총 상품금액: </span>
+                <span className="total-price" value="0">
+                  0
+                </span>
+                <span className="won">원</span>
+              </div>
+              <form className="mark-btn">
+                <button className="restock">재입고 알림</button>
+                <button className="buy-often"> 늘 사는 것</button>
+                <button className="add-cart-btn">장바구니 담기</button>
+              </form>
+            </div>
           </div>
-        </div>
-        <div></div>
+        </article>
       </section>
     );
   }
