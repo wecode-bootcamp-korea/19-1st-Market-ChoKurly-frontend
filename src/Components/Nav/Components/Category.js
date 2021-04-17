a342942633aa69f46df86133281b39a0a7ed4825
Jessica Lee <to.jessywlee@gmail.com>
2021-04-17 19:04:02 +0900

    ProductList Branch 커밋

---
+++ src/Components/Nav/Components/Category.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Category.scss';
+
+class Category extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categoryList: [],
+      isSubOpen: false,
+      categoryIdx: '',
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/data/category.json', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(categoryData => {
+        this.setState({
+          categoryList: categoryData,
+        });
+      });
+  }
+
+  handleSubOn = e => {
+    this.setState({
+      isSubOpen: true,
+      categoryIdx: e.target.id,
+    });
+  };
+
+  handleSubOff = e => {
+    this.setState({
+      isSubOpen: false,
+    });
+  };
+
+  render() {
+    const { categoryList, isSubOpen, categoryIdx } = this.state;
+    const { handleSubOn, handleSubOff } = this;
+    const { handleCategoryOff, navBarFixed } = this.props;
+
+    return (
+      <div
+        className="category-container"
+        id={navBarFixed ? 'category-fixed' : null}
+        onMouseLeave={handleCategoryOff}
+      >
+        <ul className="category-all">
+          {categoryList.map((item, idx) => {
+            return (
+              <li
+                className="mapped-category"
+                key={item.categoryId}
+                onMouseEnter={handleSubOn}
+                id={item.categoryId}
+              >
+                <div className="cat" id={item.categoryId}>
+                  <i>{item.categoryIcon}</i>
+                  <Link id={item.categoryId}>{item.categoryName}</Link>
+                </div>
+                {idx + 1 === Number(categoryIdx) && isSubOpen && (
+                  <div className="subcategory-list" onMouseLeave={handleSubOff}>
+                    {categoryList[categoryIdx - 1].subcategory.map(sub => {
+                      return (
+                        <div className="sub">
+                          <Link to="/productlist">{sub.subcategoryName}</Link>
+                        </div>
+                      );
+                    })}
+                  </div>
+                )}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Category;
---
+++ src/Pages/ProductList/ProductList.js
@@ -0,0 +1,125 @@
+import React, { Component } from 'react';
+import Nav from '../../Components/Nav/Nav';
+import ProductCard from './Components/ProductCard';
+import Modal from './Components/Modal';
+import { Link } from 'react-router-dom';
+import './ProductList.scss';
+
+class ProductList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      productList: [],
+      clickCartButton: false,
+      cartNumber: 0,
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/data/productCard.json', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(productData => {
+        this.setState({
+          productList: productData,
+        });
+      });
+  }
+
+  openCart = () => {
+    this.setState({
+      clickCartButton: true,
+    });
+  };
+
+  closeCart = () => {
+    this.setState({
+      clickCartButton: false,
+    });
+  };
+
+  addToCart = () => {
+    const { cartNumber } = this.state;
+    this.setState({
+      clickCartButton: false,
+      cartNumber: cartNumber + 1,
+    });
+  };
+
+  render() {
+    const { productList, cartNumber, clickCartButton } = this.state;
+    const { addToCart, closeCart, openCart } = this;
+
+    return (
+      <>
+        <Nav cartNumber={cartNumber} />
+        <div className="product-list-container">
+          <div className="banner">
+            <img
+              className="banner-img"
+              src="/images/ProductList/snack.jpg"
+            ></img>
+          </div>
+          <div className="product-list-menu">
+            <div className="category-title">
+              <i className="fas fa-cookie-bite fa-lg"></i>간식･과자･떡
+            </div>
+            <div className="sub-title">
+              <div className="sub-names">
+                <Link to="/main" className="sub-name">
+                  과자･스낵･쿠키
+                </Link>
+                <Link to="/main" className="chocolate sub-name">
+                  초콜릿･젤리･캔디
+                </Link>
+                <Link to="/main" className="sub-name">
+                  떡･한과
+                </Link>
+                <Link to="/main" className="sub-name">
+                  아이스크림
+                </Link>
+              </div>
+              <div className="filter-container">
+                <select className="filter" name="추천순">
+                  <option>혜택순</option>
+                  <option>낮은가격순</option>
+                  <option>높은가격순</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div className="product-list">
+            {productList.map((product, index) => {
+              return (
+                <div key={index} className="product-container">
+                  <ProductCard
+                    product={product}
+                    productSize={{
+                      height: '395px',
+                      width: '310px',
+                    }}
+                    productBorder={{
+                      border: '1px solid #ddd',
+                    }}
+                    fontStyle={{ fontSize: '20px' }}
+                    fontStyle2={{ fontSize: '18px' }}
+                    productMargin={{ marginBottom: '100px' }}
+                  />
+                  <button onClick={openCart} className="cart-button">
+                    <i className="fas fa-shopping-cart fa-lg"></i>
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+          {clickCartButton && (
+            <Modal addToCart={addToCart} closeCart={closeCart} />
+          )}
+        </div>
+      </>
+    );
+  }
+}
+
+export default ProductList;
