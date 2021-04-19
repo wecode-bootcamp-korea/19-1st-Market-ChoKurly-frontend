01d65be90dee4a0c5a2c0687601306116a6f7b84
Jessica Lee <to.jessywlee@gmail.com>
2021-04-19 18:08:33 +0900

    Modify: 상품 컴포넌트 수정2

--- src/Pages/Main/Components/ProductCard.js
+++ src/Pages/Main/Components/ProductCard.js
@@ -43,7 +43,7 @@ class ProductCard extends Component {
               <span>{product.name}</span>
             </Link>
             <div className="listed-price" style={fontStyle2}>
-              {product.sticker && (
+              {product.discount && (
                 <span className="product-discount">
                   {(Number(product.discount) * 100).toLocaleString('en-US')}%
                 </span>
