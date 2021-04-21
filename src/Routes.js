1849d694b92182d8cdca2b63fb19dec1552f02de
Jessica Lee <to.jessywlee@gmail.com>
2021-04-21 13:23:12 +0900

    routes conflict 수정

--- src/Routes.js
+++ src/Routes.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Main from './Pages/Main/Main';
 import Signup from './Pages/Signup/Signup';
-// import Signupfin from './Pages/Signup/SignupFin/SignupFin';
+import Signupfin from './Pages/Signup/SignupFin/SignupFin';
 import Login from './Pages/Login/Login';
 import ProductList from './Pages/ProductList/ProductList';
 import ProductDetails from './Pages/ProductDetails/ProductDetails';
@@ -18,7 +18,7 @@ class Routes extends Component {
         <Switch>
           <Route exact path="/main" component={Main} />
           <Route exact path="/signup" component={Signup} />
-          {/* <Route exact path="/signupfin" component={Signupfin} /> */}
+          <Route exact path="/signupfin" component={Signupfin} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/productlist" component={ProductList} />
           <Route exact path="/productdetails" component={ProductDetails} />
