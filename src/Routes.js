506334a4774b4f49bf81608ebea532ad547bd4b3
Jessica Lee <to.jessywlee@gmail.com>
2021-04-21 13:21:25 +0900

    common scss, routes js 수정

--- src/Routes.js
+++ src/Routes.js
@@ -3,10 +3,13 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Main from './Pages/Main/Main';
 import Signup from './Pages/Signup/Signup';
+import Signupfin from './Pages/Signup/SignupFin/SignupFin';
 import Login from './Pages/Login/Login';
 import ProductList from './Pages/ProductList/ProductList';
 import ProductDetails from './Pages/ProductDetails/ProductDetails';
 import Cart from './Pages/Cart/Cart';
+
+import './Styles/common.scss';
 
 class Routes extends Component {
   render() {
@@ -15,6 +18,7 @@ class Routes extends Component {
         <Switch>
           <Route exact path="/main" component={Main} />
           <Route exact path="/signup" component={Signup} />
+          <Route exact path="/signupfin" component={Signupfin} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/productlist" component={ProductList} />
           <Route exact path="/productdetails" component={ProductDetails} />
